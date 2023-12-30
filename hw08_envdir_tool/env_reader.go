c7bbcff867d3586ec46b30e31f871f01cda8720a
uralmetal <uralmetal1@yandex.ru>
2023-12-30 09:13:05 +0300

    HW08 envdir tool

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +15,45 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func readFile(path string) (EnvValue, error) {
+	valueRaw, err := os.ReadFile(path)
+	if err != nil {
+		return EnvValue{}, err
+	}
+	needRemove := len(valueRaw) == 0
+	valueLines := strings.SplitN(string(valueRaw), "\n", 2)
+	valueStr := strings.ReplaceAll(valueLines[0], "\x00", "\n")
+	valueStr = strings.TrimRight(valueStr, " \t")
+
+	return EnvValue{
+		Value:      valueStr,
+		NeedRemove: needRemove,
+	}, nil
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		fileName := file.Name()
+		if strings.ContainsRune(fileName, '=') {
+			fmt.Println("Skip invalid filename", fileName, "in dir", dir)
+			continue
+		}
+		value, err := readFile(filepath.Join(dir, fileName))
+		if err != nil {
+			fmt.Println("Error read file", fileName, ":", err)
+			continue
+		}
+		env[fileName] = value
+	}
+	return env, nil
 }

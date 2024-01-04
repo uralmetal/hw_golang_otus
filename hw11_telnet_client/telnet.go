24a887dc25374a42a3b42cf4d438f4aad69c4048
uralmetal <uralmetal1@yandex.ru>
2024-01-04 14:18:47 +0300

    feat: first version hw11 telnet client

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +13,49 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type telnetClient struct {
+	TelnetClient
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (client *telnetClient) Connect() (err error) {
+	client.conn, err = net.DialTimeout("tcp", client.address, client.timeout)
+	return
+}
+
+func (client *telnetClient) Send() (err error) {
+	buffer := make([]byte, 1024)
+	n, err := client.in.Read(buffer)
+	if err != nil {
+		return
+	}
+	_, err = client.conn.Write(buffer[:n])
+	return
+}
+
+func (client *telnetClient) Receive() (err error) {
+	buffer := make([]byte, 1024)
+	n, err := client.conn.Read(buffer)
+	if err != nil {
+		return
+	}
+	_, err = client.out.Write(buffer[:n])
+	return
+}
+
+func (client *telnetClient) Close() (err error) {
+	return client.conn.Close()
+}
